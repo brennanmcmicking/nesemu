1e2707cc23294ec80dd339745b41607ae992f44c
Anika Bookout <anbookout@gmail.com>
2023-07-19 20:13:05 -0700

    GLFWWindow is opaque... why god

--- include/ppu.hpp
+++ include/ppu.hpp
@@ -1,17 +1,47 @@
+#include <GLFW/glfw3.h>
+
+#include <array>
+#include <cstdint>
+#include <memory>
+
 #ifndef PPU_HPP
 #define PPU_HPP
-// i have no idea what to put here
-// anika do your thing
 namespace ppu {
 
 class PPU {
  public:
-  PPU();  // placeholder
+  // Data structures to store PPU output as an intermediate representation
+  // before drawing a frame to screen
+
+  static const int kNumScanlines = 262;  // pixel height, including vblank
+  static const int kScreenWidth = 256;   // pixels
+  static const int kScreenHeight = 240;  // pixels
+  // A pixel's color on the NES is one byte to describe its hue &
+  // value/brightness
+  using pixel_t = uint8_t;
+  using frame_t = std::array<pixel_t, kScreenWidth * kScreenHeight>;
+
+  PPU();
+  ~PPU();
 
   PPU(PPU&) = delete;
   PPU(PPU&&) = delete;
   PPU& operator=(PPU&) = delete;
   PPU operator=(PPU&&) = delete;
+
+  // Interface: takes data from cpu bus, formats it in a pixel buffer, then
+  // sends it to the output: a window (but ideally can be extensible)
+
+  void init_window();
+
+  void render_frame(frame_t& out);
+
+ private:
+  // Handle to the window. Nullptr before the window is created
+  using window_ptr_t = std::unique_ptr<GLFWwindow, void (*)(GLFWwindow*)>;
+  window_ptr_t window_;
+
+  // GLFWwindow* window_;
 };
 
 }  // namespace ppu
--- lib/ppu/ppu.cpp
+++ lib/ppu/ppu.cpp
@@ -1,6 +1,52 @@
 #include "ppu.hpp"
 
+#include <GLFW/glfw3.h>
+
+#include <boost/log/trivial.hpp>
+
 namespace ppu {
-// TODO
-PPU::PPU(){};
+PPU::PPU()
+    : window_(
+          glfwCreateWindow(kScreenWidth, kScreenHeight, "nesemu", NULL, NULL),
+          &glfwDestroyWindow) {
+  // Function called whenever glfw errors
+  glfwSetErrorCallback([](int err, const char* desc) {
+    BOOST_LOG_TRIVIAL(error) << "GLFW error" << err << ": " << desc << "\n";
+  });
+  // Setup window
+  // init_window();
+};
+
+PPU::~PPU() {
+  // Cleanup glfw: windows and everything
+  glfwTerminate();
 }
+
+void PPU::init_window() {
+  if (window_) {
+    // Window was already created
+    BOOST_LOG_TRIVIAL(warning) << "Window already initialized\n";
+    return;
+  }
+
+  if (!glfwInit()) {
+    BOOST_LOG_TRIVIAL(fatal) << "Could not initialize window\n";
+    glfwTerminate();
+    exit(1);
+  }
+
+  // GLFWwindow* window =
+  //     glfwCreateWindow(kScreenWidth, kScreenHeight, "nesemu", NULL, NULL);
+
+  if (!window_) {
+    // Window creation failed
+    BOOST_LOG_TRIVIAL(fatal) << "GLFW window creation failed\n";
+  }
+
+  // window_ = window;
+  // window_ = std::make_unique<GLFWwindow>(
+  //     glfwCreateWindow(kScreenWidth, kScreenHeight, "nesemu", NULL, NULL),
+  //     &glfwDestroyWindow);
+}
+
+}  // namespace ppu
