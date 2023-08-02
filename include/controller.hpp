6cf7e5b522293b45b5b2fa2d896216cc48130589
Malcolm Seyd <malcolm@seyd.ca>
2023-08-02 16:12:43 -0700

    remove unnecessary  from controller interface

--- include/controller.hpp
+++ include/controller.hpp
@@ -11,14 +11,10 @@ class Controller {
   ~Controller();
   void write_strobe(uint8_t value);
   uint8_t read_joy1();
-  uint8_t read_joy2();
 
  private:
   GLFWwindow& window_;
-
-  // Input joy1_input_;
   uint8_t joy1_register_;
-
   bool strobe_;
 };
 }  // namespace controller
